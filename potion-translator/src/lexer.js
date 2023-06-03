5939cc1d0d21c4679c252d579b019b00a0ffb511
Matt Williams <thatjsguy555@gmail.com>
2023-06-03 00:49:53 -0600

    Create lexer.js

---
+++ potion-translator/src/lexer.js
@@ -0,0 +1,43 @@
+// Token types
+const TokenType = {
+  IDENTIFIER: 'IDENTIFIER',
+  KEYWORD: 'KEYWORD',
+  STRING: 'STRING',
+  NUMBER: 'NUMBER',
+  SYMBOL: 'SYMBOL',
+  INDENT: 'INDENT',
+  DEDENT: 'DEDENT',
+  NEWLINE: 'NEWLINE',
+  EOF: 'EOF',
+};
+
+// Token class
+class Token {
+  constructor(type, value, line) {
+    this.type = type;
+    this.value = value;
+    this.line = line;
+  }
+}
+
+// Lexer class
+class Lexer {
+  constructor(code) {
+    this.code = code;
+    this.tokens = [];
+    this.indentStack = [0];
+    this.currentIndent = 0;
+    this.line = 1;
+    this.position = 0;
+    this.current = '';
+  }
+
+  tokenize() {
+    // Your lexer code here
+  }
+
+  // Helper methods for tokenizing, such as processIndentation, skipComment, processString, etc.
+}
+
+// Export the Lexer class
+module.exports = Lexer;
